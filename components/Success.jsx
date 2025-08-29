b3690d65495b64756d4c966b84a9b7e1a9d10161
Ajith7736 <ajith.aju39502@gmail.com>
2025-08-29 23:38:06 +0530

    Update Success.jsx

--- components/Success.jsx
+++ components/Success.jsx
@@ -29,7 +29,7 @@ function Success() {
 
   return (
     <div>
-      <div className='text-xl max-[450px]:text-xs font-bold -top-20 p-4 absolute w-full shadow-2xl bg-gray-900 success-message flex justify-between items-center'>Successfully completed this lab <Link href={"/Labs"} className='bg-white/5 p-2 rounded-lg border border-gray-600' onClick={handleclick}>Go back</Link></div>
+      <div className='text-xl max-[450px]:text-sm font-bold -top-20 p-4 absolute w-full shadow-2xl bg-gray-900 success-message flex justify-between items-center'>Successfully completed this lab <Link href={"/Labs"} className='bg-white/5 p-2 rounded-lg border border-gray-600' onClick={handleclick}>Go back</Link></div>
     </div>
   )
 }
