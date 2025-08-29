7e0b7497318b2d2239a55775bc187738e92eb928
Ajith7736 <ajith.aju39502@gmail.com>
2025-08-29 23:41:49 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FaFacebook } from "react-icons/fa";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { FaInstagramSquare } from "react-icons/fa";
 import { AiFillTwitterSquare } from "react-icons/ai";
@@ -9,7 +8,6 @@ function Footer() {
     return (
         <div className='mt-10 bg-white/5 border-t border-gray-700 p-5 w-full'>
             <div className='w-full flex justify-center gap-6'>
-                <FaFacebook className='size-8 max-[450px]:size-4 cursor-pointer' />
                 <AiFillGoogleCircle className='size-9 max-[450px]:size-5 cursor-pointer' />
                 <FaInstagramSquare className='size-9 max-[450px]:size-5 cursor-pointer' />
                 <AiFillTwitterSquare className='size-9 max-[450px]:size-5 cursor-pointer' />
